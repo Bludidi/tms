8906318ce1295a202b2da72a56ebcb9180ef039a
Bludidi <bludidi5@gmail.com>
2024-10-20 21:20:41 +0200

    Create new erb file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,11 +15,18 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save!
+     # render status: :ok, json: { notice: 'User created successfully' }
+      redirect_to :action => 'index'
+    else render :action => 'create', notice: 'Something went wrong'
+    end
   end
 
   def delete
     @user = User.find(params[:id]).destroy
   end
+
+  # private 
 
   def user_params
     params.require(:user).permit(:name, :email, :password)
